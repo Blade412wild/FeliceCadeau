ba202e659d127b84266c919c0eb2fd1c6498a31f
Blade412wild <nathanhoh951@gmail.com>
2023-12-21 09:57:51 +0100

    Added pickUp mechanic

---
+++ Felice/Assets/01_Scripts/Plug and Socket/GateManager.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateManager : MonoBehaviour
+{
+    public GameObject FrontGate;
+    public GameObject CenterGate;
+    public GameObject MainGate;
+
+    private bool SocketPurple;
+    private bool SocketOrange;
+
+    public delegate void SocketActivated();
+    public static event SocketActivated OnOpenMainGate;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Socket.OnplughasBeenPlaced += GateHandler;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void GateHandler(Socket.SocketType _socketType)
+    {
+        switch (_socketType)
+        {
+            case Socket.SocketType.Blue: OpenFrontGate(); break;
+            case Socket.SocketType.Green: OpenMainGate(); break;
+            case Socket.SocketType.purple: SocketPurple = true; OpenCenterGate(); break;
+            case Socket.SocketType.Orange: SocketOrange = true; OpenCenterGate(); break;
+        }
+
+
+    }
+
+    private void OpenFrontGate()
+    {
+        Destroy(FrontGate);
+    }
+
+    private void OpenMainGate()
+    {
+        OnOpenMainGate?.Invoke();
+    }
+
+    private void OpenCenterGate()
+    {
+        if (SocketOrange == true && SocketPurple == true)
+        {
+            Destroy(CenterGate);
+        }
+    }
+    private void OnDisable()
+    {
+        Socket.OnplughasBeenPlaced -= GateHandler;
+    }
+}
